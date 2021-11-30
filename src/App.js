e805e084e2034accb7dfc092ddb7a249793070d3
Matt Dudzik <mdudzik92@gmail.com>
2021-11-29 23:57:30 -0600

    Setting showNotification for wrong letters entered

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Header from './components/Header';
 import Figure from './components/Figure';
 import WrongLetters from './components/WrongLetters';
 import Word from './components/Word';
+import Notification from './components/Notification';
+import Popup from './components/Popup';
+import {showNotification as show} from './helpers/helpers';
 
 const words = ['application', 'programming', 'interface', 'wizard'];
 
@@ -14,6 +17,7 @@ function App() {
   const [playable, setPlayable] = useState(true);
   const [correctLetters, setCorrectLetters] = useState([]);
   const [wrongLetters, setWrongLetters] = useState([]);
+  const [showNotification, setShowNotification] = useState(false);
 
   useEffect(() => {
     const handleKeyDown = event => {
@@ -25,13 +29,13 @@ function App() {
             if (!correctLetters.includes(letter)) {
               setCorrectLetters(currentLetters => [...currentLetters, letter]);
             } else {
-              // showNotification();
+              show(setShowNotification);
             }
           } else {
             if (!wrongLetters.includes(letter)) {
               setWrongLetters(wrongLetters => [...wrongLetters, letter]);
             } else {
-              // showNotification();
+              show(setShowNotification);
             }
           }
         }
@@ -47,9 +51,11 @@ function App() {
     <>
       <Header />
       <div className="game-container">
-        <Figure />
+        <Figure wrongLetters={wrongLetters} />
         <WrongLetters wrongLetters={wrongLetters} />
         <Word selectedWord={selectedWord} correctLetters={correctLetters} />
+        <Popup />
+        <Notification showNotification={showNotification} />
       </div>
     </>
   );
